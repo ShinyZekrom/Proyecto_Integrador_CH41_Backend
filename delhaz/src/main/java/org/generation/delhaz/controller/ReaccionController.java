cf5e3bfe21b80d52aa074549cef91d12f3143518
Zekrom <maldonado.luis.iqm@gmail.com>
2024-07-03 21:41:34 -0600

    Se crea método POST faltante para ReaccionController y Service

--- delhaz/src/main/java/org/generation/delhaz/controller/ReaccionController.java
+++ delhaz/src/main/java/org/generation/delhaz/controller/ReaccionController.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import org.generation.delhaz.model.Reaccion;
 import org.generation.delhaz.service.ReaccionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +37,15 @@ public class ReaccionController {
         return reaccionService.getReaction(id);
     }
 
-    //no se añade una nueva reacción debido a que se maneja en la tabla de TipoReaccion
+    @PostMapping
+    public ResponseEntity<Reaccion> addReaccion(@RequestBody Reaccion reaccion) {
+        Reaccion nuevaReaccion = reaccionService.addReaction(reaccion);
+        if (nuevaReaccion != null) {
+            return new ResponseEntity<>(nuevaReaccion, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
     
     @DeleteMapping(path="{reaccionId}")
     public Reaccion deleteReaction(@PathVariable("reaccionId") int id) {
