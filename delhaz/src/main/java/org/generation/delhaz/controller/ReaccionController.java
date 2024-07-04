1ec1f8cc0529488488b773455ef91f8f93f933c2
Zekrom <maldonado.luis.iqm@gmail.com>
2024-07-03 22:52:34 -0600

    Se actualizó el método POST de ReaccionController

--- delhaz/src/main/java/org/generation/delhaz/controller/ReaccionController.java
+++ delhaz/src/main/java/org/generation/delhaz/controller/ReaccionController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import org.generation.delhaz.model.Reaccion;
 import org.generation.delhaz.service.ReaccionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,13 +36,8 @@ public class ReaccionController {
     }
 
     @PostMapping
-    public ResponseEntity<Reaccion> addReaccion(@RequestBody Reaccion reaccion) {
-        Reaccion nuevaReaccion = reaccionService.addReaction(reaccion);
-        if (nuevaReaccion != null) {
-            return new ResponseEntity<>(nuevaReaccion, HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+    public Reaccion addReaccion(@RequestBody Reaccion reaccion) {
+        return reaccionService.addReaction(reaccion);
     }
     
     @DeleteMapping(path="{reaccionId}")
